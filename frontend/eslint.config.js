5cb958862799c4c04518b4da0518cd708ef71890
Arman Shaikh <arman.shaikh.work@gmail.com>
2026-09-01 11:43:20 +0100

    fix: reorder import statements in vite.config.js for clarity

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,9 @@
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
-import globals from "globals";
 import importPlugin from "eslint-plugin-import";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 
 export default [
   {
